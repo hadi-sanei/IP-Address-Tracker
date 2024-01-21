b8391430bb52c56dc25a226f7cbba8c977afc4c0
hadi sanei <hadi.sanei.net@gmail.com>
2024-01-21 19:20:17 +0330

    feat:adding map marker to the project

--- assets/js/cod.js
+++ assets/js/cod.js
@@ -6,5 +6,21 @@
         zoomControl: false,
         maxZoom: 19,
     });
-    L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {}).addTo(map);
+    L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png').addTo(map);
+    let theMarker;
+    function addMarkerToMap(event) {
+        if (theMarker != undefined) {
+            map.removeLayer(theMarker);
+        }
+        const myIcon = L.icon({
+            iconUrl: '../../assets/images/icon-location.svg',
+            iconSize: [30, 40],
+            iconAnchor: [15, 39],
+        });
+        theMarker = L.marker(event.latlng, { icon: myIcon }).addTo(map);
+        map.setView(event.latlng, 13);
+    }
+    map.addEventListener("click", (e) => {
+        addMarkerToMap(e);
+    });
 })();
--- assets/typescript/cod.ts
+++ assets/typescript/cod.ts
@@ -1,10 +1,27 @@
-(function(){
-    const map = L.map('map',{
+(function () {
+    const map: L.Map = L.map('map', {
         center: [51.505, -0.09],
         zoom: 13,
-        zoomControl:false,
+        zoomControl: false,
         maxZoom: 19,
     });
-    L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    }).addTo(map);
+    L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png').addTo(map);
+    let theMarker: L.Marker;
+    function addMarkerToMap(event: L.LeafletMouseEvent) {
+        if (theMarker != undefined) {
+            map.removeLayer(theMarker);
+        }
+        const myIcon = L.icon({
+            iconUrl: '../../assets/images/icon-location.svg',
+            iconSize: [30, 40],
+            iconAnchor: [15, 39],
+        });
+        theMarker = L.marker(event.latlng, { icon: myIcon }).addTo(map);
+        map.setView(event.latlng,13);
+    }
+
+    map.addEventListener("click", (e) => {
+        addMarkerToMap(e);
+    });
+
 })();
