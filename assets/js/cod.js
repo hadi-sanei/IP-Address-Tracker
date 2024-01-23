f5860987679a498e685bcb22978f5d667a20c4e9
hadi sanei <hadi.sanei.net@gmail.com>
2024-01-23 19:39:52 +0330

    feat:loading ip to the project

--- assets/js/cod.js
+++ assets/js/cod.js
@@ -1,5 +1,18 @@
 "use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
 (function () {
+    const ip_address = document.querySelector('#IPAddress');
+    const location = document.querySelector('#location');
+    const timezone = document.querySelector('#timezone');
+    const isp = document.querySelector('#ISP');
     const map = L.map('map', {
         center: [51.505, -0.09],
         zoom: 13,
@@ -17,10 +30,27 @@
             iconSize: [30, 40],
             iconAnchor: [15, 39],
         });
-        theMarker = L.marker(event.latlng, { icon: myIcon }).addTo(map);
-        map.setView(event.latlng, 13);
+        theMarker = L.marker((event === null || event === void 0 ? void 0 : event.latlng) || [event.lat, event.lon], { icon: myIcon }).addTo(map);
+        map.setView((event === null || event === void 0 ? void 0 : event.latlng) || [event.lat, event.lon], 13);
     }
     map.addEventListener("click", (e) => {
         addMarkerToMap(e);
     });
+    function loadIpAddress() {
+        return __awaiter(this, void 0, void 0, function* () {
+            let data = yield fetch('http://ip-api.com/json');
+            data = yield data.json();
+            return data;
+        });
+    }
+    loadIpAddress().then(data => {
+        addMarkerToMap(data);
+        updateElements(data);
+    });
+    function updateElements(data) {
+        ip_address.innerHTML = data.query;
+        location.innerHTML = `${data.city}, ${data.country}`;
+        timezone.innerHTML = data.timezone;
+        isp.innerHTML = data.isp;
+    }
 })();
--- assets/typescript/cod.ts
+++ assets/typescript/cod.ts
@@ -1,4 +1,8 @@
 (function () {
+    const ip_address=document.querySelector('#IPAddress')!;
+    const location=document.querySelector('#location')!;
+    const timezone=document.querySelector('#timezone')!;
+    const isp=document.querySelector('#ISP')!;
     const map: L.Map = L.map('map', {
         center: [51.505, -0.09],
         zoom: 13,
@@ -7,7 +11,7 @@
     });
     L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png').addTo(map);
     let theMarker: L.Marker;
-    function addMarkerToMap(event: L.LeafletMouseEvent) {
+    function addMarkerToMap(event:any) {
         if (theMarker != undefined) {
             map.removeLayer(theMarker);
         }
@@ -16,12 +20,29 @@
             iconSize: [30, 40],
             iconAnchor: [15, 39],
         });
-        theMarker = L.marker(event.latlng, { icon: myIcon }).addTo(map);
-        map.setView(event.latlng,13);
+        theMarker = L.marker(event?.latlng || [event.lat,event.lon], { icon: myIcon }).addTo(map);
+        map.setView(event?.latlng || [event.lat,event.lon],13);
     }
 
-    map.addEventListener("click", (e) => {
+    map.addEventListener("click", (e:Object) => {
         addMarkerToMap(e);
     });
 
+    async function loadIpAddress(){
+        let data=await fetch('http://ip-api.com/json');
+        data=await data.json();
+        return data;
+    }
+    
+    loadIpAddress().then(data=>{
+        addMarkerToMap(data);
+        updateElements(data);
+    });
+
+    function updateElements(data:any){
+        ip_address.innerHTML= data.query;
+        location.innerHTML= `${data.city}, ${data.country}`;
+        timezone.innerHTML= data.timezone;
+        isp.innerHTML= data.isp;
+    }
 })();
